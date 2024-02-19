62147f916e044cf297548cca4dcbe3faa0fabf01
tamistyping <enquiretam@gmail.com>
2024-02-19 11:28:41 +0000

    CRUD functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 finches = [
   {'name': 'John', 'species': 'Zebra Finch', 'description': 'Cheerful and chirpy', 'age': 1},
@@ -22,3 +23,17 @@ def finches_index(request):
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    # fields is same as saying ['name', 'breed'. 'desc' etc]
+    success_url = '/finches/{id}'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['species', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
