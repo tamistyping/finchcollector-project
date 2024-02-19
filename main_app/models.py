5d303cf72e07511a89baee4f01b6fae668b387b4
tamistyping <enquiretam@gmail.com>
2024-02-19 20:02:19 +0000

    CRUD functions

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -12,3 +18,19 @@ class Finch(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+class Feeding(models.Model):
+    date = models.DateField(("feeding date"))
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Finch
+from .forms import FeedingForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 finches = [
@@ -22,7 +23,16 @@ def finches_index(request):
 
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', {'finch': finch})
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form':feeding_form})
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
     model = Finch
