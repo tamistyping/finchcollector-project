5a58bf3f5ff804477b0ea47b019e31d4b0e0f7ef
tamistyping <enquiretam@gmail.com>
2024-02-20 14:51:59 +0000

    many to many model

--- main_app/models.py
+++ main_app/models.py
@@ -7,11 +7,22 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
     
     def __str__(self):
         return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Finch
+from .models import Finch, Toy
 from .forms import FeedingForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
 finches = [
   {'name': 'John', 'species': 'Zebra Finch', 'description': 'Cheerful and chirpy', 'age': 1},
@@ -23,8 +24,21 @@ def finches_index(request):
 
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  id_list = finch.toys.all().values_list('id')
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
   feeding_form = FeedingForm()
-  return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form':feeding_form})
+  return render(request, 'finches/detail.html', {
+    'finch': finch, 'feeding_form':feeding_form,
+    'toys': toys_finch_doesnt_have              
+  })
+  
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def remove_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
 
 def add_feeding(request, finch_id):
   form = FeedingForm(request.POST)
@@ -37,7 +51,6 @@ def add_feeding(request, finch_id):
 class FinchCreate(CreateView):
     model = Finch
     fields = '__all__'
-    # fields is same as saying ['name', 'breed'. 'desc' etc]
     success_url = '/finches/{id}'
 
 class FinchUpdate(UpdateView):
@@ -47,3 +60,21 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finches'
+  
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
